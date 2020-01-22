a77cfad927e626efda41a360845c13229f851f3d
SabaKhangholi <55306943+SabaKhangholi@users.noreply.github.com>
2020-01-21 18:43:18 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// main.cpp
+
+#include "StringSet.h"
+#include <iostream>	// FIX LATER
+#include<string>
+using std::string; // FIX LATER
+using std::iostream;
+using std::cout;
+using std::endl; // FIX LATER
+
+int main()
+{
+
+	// TEST: string set
+	StringSet(testInsert);
+	cout << "Max array size: " << testInsert.getMaxArraySize() << endl;
+	cout << "Current array size: " << testInsert.getSetSize() << endl;
+	// END TEST
+
+	// TEST: Copy constructor
+	//testInsert.insert("hi");
+	testInsert.insert("n");
+	testInsert.insert("n");
+	testInsert.insert("n");
+	cout << "new array size: "<<testInsert.getSetSize() << endl;
+	cout << "new max array size: " << testInsert.getMaxArraySize() << endl;
+	//cout << string.insert("hi");
+	//cout << string1;
+	
+	// END TEST
+
+	return 0;
+}
